60416fbce7151fbcf0d08c80f0df1e8851950474
Job PC <jobbouwhuis@hotmail.com>
2025-11-06 23:37:23 +0100

    guid value provider now included by default

---
+++ src/WinterRose.WinterForge/BuildInValueProviders/GuidValueProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinterRose.WinterForgeSerializing;
+using WinterRose.WinterForgeSerializing.Workers;
+
+namespace WinterRose.WinterForgeSerializing
+{
+    internal class GuidValueProvider : CustomValueProvider<Guid>
+    {
+        public override Guid CreateObject(object value, WinterForgeVM executor) => Guid.Parse((string)value);
+        public override object CreateString(Guid obj, ObjectSerializer serializer) => obj.ToString();
+    }
+}
