608b51e02d61d6e63687dd0e4e321712799b6436
Job PC <jobbouwhuis@hotmail.com>
2026-06-30 00:04:14 +0200

    an extra builtin converter

---
+++ src/WinterRose.Reflection/TypeConverters/Builtin/ByteToChar.cs
@@ -0,0 +1,17 @@
+﻿namespace WinterRose.Reflection.TypeConverters.Builtin;
+
+public class ByteToChar : TypeConverter<char, byte>
+{
+    public override byte Convert(char source)
+    {
+        return (byte)source;
+    }
+}
+
+public class CharToByte : TypeConverter<byte, char>
+{
+    public override char Convert(byte source)
+    {
+        return (char)source;
+    }
+}
